f01f309d13539c59f626d3fe97636ebe04739b5f
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2025-02-18 10:51:02 -0300

    Add files via upload

---
+++ React04/App4.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hora: "00:00:00",
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log("Componente re-renzerizado!");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.hora}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
