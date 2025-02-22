ce64ed7f98a11190868fe24d0cea96b15d45fcb2
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2025-02-22 12:15:15 -0300

    Update script.js

--- projeto_imc/script.js
+++ projeto_imc/script.js
@@ -8,13 +8,20 @@ function calcularImc(event) {
 
   let imc = peso / altura ** 2;
 
-  if (imc < 18.5) {
+  if (imc < 18.5) 
+  {  
     area.innerHTML = `Magreza, seu imc é de: ${imc.toFixed(1)}`;
-  } else if (imc > 18.5 && imc < 24.9) {
+  }
+  else if (imc > 18.5 && imc < 24.9) 
+  {
     area.innerHTML = `Normal, seu imc é de: ${imc.toFixed(1)}`;
-  } else if (imc > 24.9 && imc < 30) {
+  } 
+  else if (imc > 24.9 && imc < 30) 
+  {
     area.innerHTML = `Sobrepeso, seu imc é de: ${imc.toFixed(1)}`;
-  } else if (imc >= 30) {
+  }
+  else if (imc >= 30)
+  {
     area.innerHTML = `Obesidade, seu imc é de: ${imc.toFixed(1)}`;
   }
 
